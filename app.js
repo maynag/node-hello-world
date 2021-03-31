4c070ca4330ff9c50c89dc4e48385f2770bdf89c
maynag <70300031+maynag@users.noreply.github.com>
2021-03-31 16:04:29 -0700

    completed assignment 1 changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 const server  = http.createServer((request, response) => {
 	response.statusCode = 200;
 	response.setHeader('Content-Type', 'text/plain');
-	response.end('Hello World\n\n\nYou Guys Rock!');
+	response.end('Welcome to my Web Application\n\n\nMy name is: Gregory Maynard');
 });
 
 server.listen(port, (err) => {
