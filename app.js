b03f2abf10aecfbace41fc217bcf2cf9bced90f7
maynag <70300031+maynag@users.noreply.github.com>
2021-03-31 15:52:46 -0700

    finished app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const port = process.env.PORT || 5000;
+const server  = http.createServer((request, response) => {
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/plain');
+	response.end('Hello World\n\n\nYou Guys Rock!');
+});
+
+server.listen(port, (err) => {
+	if (err) {
+		return console.log('Something went wrong ', err);
+	}
+	console.log('Server Listening on ${port}');
+});
